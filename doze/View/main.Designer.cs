58cc7b6063fb9479ce329056dcc5208f98d1303d
mayandra.vmota <mayandra.vmota@SBC0505247W10-1>
2022-05-13 17:29:21 -0300

    Adicionar arquivos de projeto.

---
+++ doze/View/main.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace doze.View
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxPara = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxEndereco = new System.Windows.Forms.TextBox();
+            this.tbxDe = new System.Windows.Forms.TextBox();
+            this.btnTroca = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "De";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Para";
+            // 
+            // tbxPara
+            // 
+            this.tbxPara.Location = new System.Drawing.Point(288, 168);
+            this.tbxPara.Name = "tbxPara";
+            this.tbxPara.Size = new System.Drawing.Size(114, 23);
+            this.tbxPara.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(284, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Endereço";
+            // 
+            // tbxEndereco
+            // 
+            this.tbxEndereco.Location = new System.Drawing.Point(288, 80);
+            this.tbxEndereco.Name = "tbxEndereco";
+            this.tbxEndereco.Size = new System.Drawing.Size(114, 23);
+            this.tbxEndereco.TabIndex = 1;
+            // 
+            // tbxDe
+            // 
+            this.tbxDe.Location = new System.Drawing.Point(288, 124);
+            this.tbxDe.Name = "tbxDe";
+            this.tbxDe.Size = new System.Drawing.Size(114, 23);
+            this.tbxDe.TabIndex = 1;
+            // 
+            // btnTroca
+            // 
+            this.btnTroca.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTroca.Location = new System.Drawing.Point(412, 194);
+            this.btnTroca.Name = "btnTroca";
+            this.btnTroca.Size = new System.Drawing.Size(58, 25);
+            this.btnTroca.TabIndex = 2;
+            this.btnTroca.Text = "Finalizar";
+            this.btnTroca.UseVisualStyleBackColor = false;
+            this.btnTroca.Click += new System.EventHandler(this.btnTroca_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.White;
+            this.btnPesquisar.Location = new System.Drawing.Point(412, 80);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(58, 25);
+            this.btnPesquisar.TabIndex = 2;
+            this.btnPesquisar.Text = "...";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnTroca);
+            this.Controls.Add(this.tbxDe);
+            this.Controls.Add(this.tbxEndereco);
+            this.Controls.Add(this.tbxPara);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "main";
+            this.Text = "main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxPara;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxEndereco;
+        private System.Windows.Forms.TextBox tbxDe;
+        private System.Windows.Forms.Button btnTroca;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ doze/View/main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace doze.View
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            tbxEndereco.Text = string.Empty;
+            try
+            {
+                if (folderBrowserDialog1.ShowDialog() != DialogResult.Cancel)
+                {
+                    tbxEndereco.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Alerta de erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+
+        }
+
+        private void btnTroca_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbxEndereco.Text.Trim() == string.Empty)
+                {
+                    throw new Exception(
+                        "selecione uma pasta para efetivar a troca de arquivo");
+                }if(tbxEndereco.Text.Trim() == string.Empty)
+                {
+                    throw new Exception("Informe o valor procurado a troca");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
